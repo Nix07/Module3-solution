e0047691152709e9c471b22fcf49a062a946577f
nix07 <nikhil07prakash@gmail.com>
2017-04-08 10:52:55 +0530

    Added Ajax Request

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 (function () {
 	'use strict';
 
-	angular.module('myapp',[]);
+	angular.module('NarrowItDownApp',[])
+	.controller('NarrowItDownController', NarrowItDownController)
+	.service('MenuSearchService', MenuSearchService);
+
+	NarrowItDownController.$inject = ['MenuSearchService'];
+	function NarrowItDownController(MenuSearchService) {
+		var ctrl = this;
+
+		ctrl.sendRequest = function(){
+			var promise = MenuSearchService.getMatchedMenuItems();
+
+			promise.then(function(response){
+				console.log(response.data);
+			})
+			.catch(function() {
+				console.log("Something went wrong!");
+			})
+		}
+	}
+
+	MenuSearchService.$inject = ['$http'];
+	function MenuSearchService($http) {
+		var service = this;
+
+		service.getMatchedMenuItems = function (){
+			var response = $http({
+				method: 'GET',
+				url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+			});
+
+			return response; 
+		}
+	}
 
 })();
