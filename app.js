888b4c60188753daaa9f111bd6a8a8511b216a6f
nix07 <nikhil07prakash@gmail.com>
2017-04-08 12:26:00 +0530

    Added Searching

--- app.js
+++ app.js
@@ -9,11 +9,12 @@
 	function NarrowItDownController(MenuSearchService) {
 		var ctrl = this;
 
+		ctrl.searchTerm = '';
 		ctrl.sendRequest = function(){
-			var promise = MenuSearchService.getMatchedMenuItems();
+			var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
 
 			promise.then(function(response){
-				console.log(response.data);
+				//console.log(response.data);
 			})
 			.catch(function() {
 				console.log("Something went wrong!");
@@ -25,14 +26,20 @@
 	function MenuSearchService($http) {
 		var service = this;
 
-		service.getMatchedMenuItems = function (){
+		var found = [];
+		service.getMatchedMenuItems = function (searchTerm){
 			return $http({
 				method: 'GET',
 				url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
 			})
 			.then(function(response){
-				console.log("Length: " + response.data.menu_items.length);
-
+				for(var i=0;i<response.data.menu_items.length;i++) {
+					var menuItemDescription = response.data.menu_items[i].description;
+					if(menuItemDescription.toLowerCase().indexOf(searchTerm) !== -1)
+						found.push(response.data.menu_items[i]);
+				}
+				for( var i =0;i<found.length;i++)
+					console.log("Item : " + found[i].name);
 				return response;
 			});
 
